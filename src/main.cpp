9f5bf99120b92345a4e58587b14600267c3b0924
TheRetroGuy2020 <sandslinger14@gmail.com>
2023-11-23 22:11:22 -0800

    Added INA219

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
+#include <Adafruit_INA219.h>
 #include <SPI.h>
 
 int batteryPercentage;
+
+Adafruit_INA219 ina219;
+
+float busvoltage = 0;
+float current_mA = 0;
+float power_mW = 0;
 
 struct timeval lastBatteryUpdate;
 
@@ -19,80 +26,43 @@ Arduino_GFX *gfx = new Arduino_ST7789(bus, 19 /* RST */, 1 /* rotation */, true 
 
 // put function declarations here:
 
-void drawParticleCircle(int percentage) {
-  int centerX = 320 / 2;
-  int centerY = 172 / 2;
-  int radius = 50;
-  
-  // Calculate the angle based on the percentage
-  float angle = map(percentage, 0, 100, 0, 360);
-  angle = angle; 
-  
-  Serial.printf("Angle: %f\n", angle);
-  // Draw the empty circle
-  gfx->drawCircle(centerX, centerY, radius+5, WHITE);
-  gfx->fillCircle(centerX, centerY, radius+2, BLACK);
-  // Draw the filled circle
-  // gfx->fillArc(centerX, centerY, radius, 0, 30, 330, GREEN);
-  if (percentage == 100){
-    gfx->fillCircle(centerX, centerY, radius, GREEN);
-  } else if (percentage >= 75 && percentage < 100){
-    gfx->fillArc(centerX, centerY, radius, 0, 10, angle, GREEN);
-  } else if (percentage >= 50 && percentage < 75){
-    gfx->fillArc(centerX, centerY, radius, 0, 10, angle, YELLOW);
-  } else if (percentage >= 25 && percentage < 50){
-    gfx->fillArc(centerX, centerY, radius, 0, 10, angle, ORANGE);
-  } else if (percentage > 0 && percentage < 25){
-    gfx->fillArc(centerX, centerY, radius, 0, 10, angle, RED);
-  } else if (percentage == 0){
-    gfx->fillCircle(centerX, centerY, radius, RED);
-  }
-}
+void measurePower(){
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
 
-float battery_voltage() {
-  int raw = analogRead(35);
-  float voltage = raw * ((float)(R1 + R2)/R2);
-  return voltage;
-}
+  Serial.printf("Voltage: %.4f\n", busvoltage);
 
-int battery_percentage() {
-  float voltage = battery_voltage();
-  int percentage = map(voltage, MAX_BATTERY_VOLTAGE, MIN_BATTERY_VOLTAGE, 0, 100);
-  return percentage;
+  Serial.printf("Current_mA: %.4f\n", current_mA);
+  Serial.printf("Power(mW): %.4f\n", power_mW);
+
 }
 
 bool is_charging() {
-  float prev = battery_voltage();
+  float prev ;
   delay(1000);
-  float curr = battery_voltage();
+  float curr;
   return curr > prev;
 }
 
-void update_battery(){
-  int percentage = battery_percentage();
-  if (percentage != batteryPercentage) {
-    batteryPercentage = percentage;
-    drawParticleCircle(batteryPercentage);
-  }
-  gettimeofday(&lastBatteryUpdate, NULL);
-}
 
 void setup() {
   Serial.begin(115200);
-  gfx->begin();
-  gfx->setRotation(1);
-  gfx->fillScreen(BLACK);
-  gfx->fillRect(0, 0, 50, 50, RED);
-  Serial.println("Hello World!");
+  Serial.println("ESP Starting");
   delay(5000);
   Serial.println("Leaving setup");
   batteryPercentage = 100;
+
+  if(!ina219.begin()){
+    Serial.println("FAILED TO FIND INA219 MODULE!");
+  }
   
   
 }
 
 void loop() {
-  Serial.printf("Starting the loop of Battery positon, given in percentage: %d\n" , batteryPercentage);
+  Serial.printf("Starting to loop using the INA219!\n");
+  measurePower();
 
   delay(5000);
 }
